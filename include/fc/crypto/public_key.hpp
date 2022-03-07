a00804aa52ef8b1963813cfdf71162bd6f7273b3
SeanChen <aws4amazon@163.com>
2022-03-07 09:44:28 +0800

    change pubkey prefix to "AM"

--- include/fc/crypto/public_key.hpp
+++ include/fc/crypto/public_key.hpp
@@ -9,7 +9,7 @@
 
 namespace fc { namespace crypto {
    namespace config {
-      constexpr const char* public_key_legacy_prefix = "AMA";
+      constexpr const char* public_key_legacy_prefix = "AM";
       constexpr const char* public_key_base_prefix = "PUB";
       constexpr const char* public_key_prefix[] = {
          "K1",
